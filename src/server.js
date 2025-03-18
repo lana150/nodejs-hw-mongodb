24c43a698ad65aadeb2e52400ec9d59be4b48d29
Lana150 <astra.altair150@gmail.com>
2025-03-18 21:47:53 +0100

    added chenges

---
+++ src/server.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
+
+const PORT = getEnvVar('PORT');
+
+export const setupServer = () => {
+  const app = express();
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.get('/contacts', async (req, res) => {
+    try {
+      const contacts = await getAllContacts();
+      res.status(200).json({ data: contacts });
+    } catch (error) {
+      res.status(500).json({ message: 'Server error', error: error.message });
+    }
+  });
+
+  app.get('/contacts/:contactId', async (req, res) => {
+    try {
+      const { contactId } = req.params;
+      const contact = await getContactById(contactId);
+
+      if (!contact) {
+        return res.status(404).json({ message: 'Contact not found' });
+      }
+
+      res.status(200).json({ data: contact });
+    } catch (error) {
+      res.status(500).json({ message: 'Server error', error: error.message });
+    }
+  });
+
+  app.use('*', (req, res) => {
+    res.status(404).json({ message: 'Not found' });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
---
+++ src/utils/getEnvVar.js
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+import process from 'process';
+
+dotenv.config();
+
+export function getEnvVar(name, defaultValue) {
+  const value = process.env[name];
+
+  if (value) return value;
+  if (defaultValue) return defaultValue;
+
+  throw new Error(`Missing: process.env['${name}'].`);
+}
