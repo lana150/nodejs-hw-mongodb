5650b7ef991dc1431ec88d07c411e7503e89f00e
Lana150 <astra.altair150@gmail.com>
2025-03-21 17:51:43 +0100

    add

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -4,7 +4,7 @@ const errorHandler = (err, req, res, next) => {
     message: 'Something went wrong',
     data: err.message || 'Unknown error',
   });
-  next();
+  
 };
 
 export default errorHandler;
