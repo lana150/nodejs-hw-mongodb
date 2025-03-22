f7df37a627570c4a70d140804492f88d733640ed
Lana150 <astra.altair150@gmail.com>
2025-03-22 22:52:44 +0100

    added changes

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,3 +1,30 @@
+import { HttpError } from 'http-errors';
+
+export const errorHandler = (err, req, res, next) => {
+  
+  if (err instanceof HttpError) {
+    res.status(err.status).json({
+      status: err.status,
+      message: err.name,
+      data: err,
+    });
+    next()
+    return;
+  }
+
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message,
+  });
+};
+
+export default errorHandler
+
+
+
+/*export default errorHandler
+
 const errorHandler = (err, req, res, next) => {
   res.status(500).json({
     status: 500,
@@ -7,4 +34,15 @@ const errorHandler = (err, req, res, next) => {
   next(err);
 };
 
-export default errorHandler;
+export default errorHandler;*/
+
+
+/*const errorHandler = (err, req, res) => {
+  res.status(500).json({
+    status: 500,
+    message: 'Something went wrong',
+    data: err.message || 'Unknown error',
+  });
+};
+
+export default errorHandler;*/
